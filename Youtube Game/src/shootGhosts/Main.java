cee2f3f2b92aed7653746ac4f61c38a6cb122b63
Evan Chen <evanturtile.chen@mail.utoronto.ca>
2018-01-23 11:40:31 -0500

    Initial upload

---
+++ Youtube Game/src/shootGhosts/Main.java
@@ -0,0 +1,7 @@
+package shootGhosts;
+
+public class Main {
+    public static void main(String[] args) {
+    	OpeningWindow ow = new OpeningWindow();
+    }
+}
---
+++ Youtube Game/src/shootGhosts/OpeningWindow.java
@@ -0,0 +1,31 @@
+package shootGhosts;
+
+import java.awt.Graphics;
+import java.awt.Toolkit;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class OpeningWindow extends JPanel {
+
+	// This is a new frame that allows us to draw this panel
+	JFrame window = new JFrame();
+
+	// Constructor Function
+	OpeningWindow() {
+		window.add(this);
+		window.setSize(1191, 670);
+		
+		window.setVisible(true);
+	}
+
+	// Allows us to draw our graphics
+	public void paint(Graphics g) {
+		// This assumes if our image exists in that location...
+		ImageIcon background = new ImageIcon("./images/grassy_plains.png");
+
+		// We draw the image starting from the top-left corner
+		g.drawImage(background.getImage(),0,0,null);
+	}
+}
